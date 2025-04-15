6f4c68d6d7ae6c50c9d5ba1a27f24187ac601352
Iksanalifi <117494236+Iksanalifi@users.noreply.github.com>
2025-04-15 11:09:50 +0700

    Create secret.h

---
+++ secret.h
@@ -0,0 +1,6 @@
+// secret.h
+#define WIFI_SSID "NamaWiFi"
+#define WIFI_PASSWORD "PasswordWiFi"
+
+#define FIREBASE_HOST "namaproject.firebaseio.com"
+#define FIREBASE_AUTH "your_firebase_database_secret_or_token"
